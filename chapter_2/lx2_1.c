06523345abbeb235cbc1641a4fa6d425ed51d663
Jinyang Shen <18618459871@163.com>
2019-05-30 09:21:11 +0800

    fix the little program

--- chapter_2/lx2_1.c
+++ chapter_2/lx2_1.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int main()
 {
-    printf("singed char min = %d\n", -(char)((unsigned char)~0 >> 1));
-    printf("singed char max = %d\n", (char)((unsigned char)~0 >> 1));
+    printf("singed char min = %d\n", -(char)((unsigned char)~0 >> 1) - 1);
+    printf("singed char min = %d\n", SCHAR_MIN);
 
-    printf("singed short min = %d\n", -(short)((unsigned short)~0 >> 1));
-    printf("singed short max = %d\n", (short)((unsigned short)~0 >> 1));
+    printf("singed char max = %d\n", (char)((unsigned char)~0 >> 1));
+    printf("singed char max = %d\n", SCHAR_MAX);
+
     return 0;
 }
